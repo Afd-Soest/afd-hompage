e6a9f199503bfc1f0f5efe39507248c95d63d61c
Afd-Soest <afd-soest@protonmail.com>
2023-12-07 18:36:56 +0100

    change Constituion arrangement

--- src/components/Constitution/Constitution.tsx
+++ src/components/Constitution/Constitution.tsx
@@ -1,5 +1,6 @@
 import Icon from "../../assets/images/Icons/link.png";
 import { links } from "./Links";
+import { Typ } from "./Typ";
 import "./Constitution.css";
 
 const Constituion = () => {
@@ -7,14 +8,82 @@ const Constituion = () => {
     <section id="constituion" className="firstSector">
       <h2 className="headline">Satzung und Ordnung</h2>
       <div className="links">
-        {links.map((item, index) => (
-          <a key={index} href={item.getLink()} target="_blank" className="link">
-            <div className="headerBx">
-              <img src={Icon} className="img"></img>
-              <h3 className="title">{item.getName()}</h3>
-            </div>
-          </a>
-        ))}
+        <div className="bezirk">
+          <div>
+            <h2>Kreissatzung</h2>
+            {links.map((item, index) =>
+              item.getTyp() == Typ.Kreis ? (
+                <a
+                  key={index}
+                  href={item.getLink()}
+                  target="_blank"
+                  className="link"
+                >
+                  <div className="headerBx">
+                    <img src={Icon} className="img"></img>
+                    <h3 className="title">{item.getName()}</h3>
+                  </div>
+                </a>
+              ) : null
+            )}
+          </div>
+          <div>
+            <h2>Bezirkssatzung</h2>
+            {links.map((item, index) =>
+              item.getTyp() == Typ.Bezirk ? (
+                <a
+                  key={index}
+                  href={item.getLink()}
+                  target="_blank"
+                  className="link"
+                >
+                  <div className="headerBx">
+                    <img src={Icon} className="img"></img>
+                    <h3 className="title">{item.getName()}</h3>
+                  </div>
+                </a>
+              ) : null
+            )}
+          </div>
+        </div>
+        <div className="bund">
+          <div>
+            <h2>Landessatzungen</h2>
+            {links.map((item, index) =>
+              item.getTyp() == Typ.Land ? (
+                <a
+                  key={index}
+                  href={item.getLink()}
+                  target="_blank"
+                  className="link"
+                >
+                  <div className="headerBx">
+                    <img src={Icon} className="img"></img>
+                    <h3 className="title">{item.getName()}</h3>
+                  </div>
+                </a>
+              ) : null
+            )}
+          </div>
+          <div>
+            <h2>Bundessatzungen</h2>
+            {links.map((item, index) =>
+              item.getTyp() == Typ.Bund ? (
+                <a
+                  key={index}
+                  href={item.getLink()}
+                  target="_blank"
+                  className="link"
+                >
+                  <div className="headerBx">
+                    <img src={Icon} className="img"></img>
+                    <h3 className="title">{item.getName()}</h3>
+                  </div>
+                </a>
+              ) : null
+            )}
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/Constitution/Links.tsx
+++ src/components/Constitution/Links.tsx
@@ -1,80 +1,92 @@
 import { ConstitutionLink } from "./ConstitutionLinks";
+import { Typ } from "./Typ";
 
 export let links: ConstitutionLink[] = [];
 
 links.push(
   new ConstitutionLink(
     "AfD KV Soest Satzung",
-    "https://www.afd-soest.de/_files/ugd/2fc31c_88705ccc71364e7f9d07408fc53401ec.pdf"
+    "https://www.afd-soest.de/_files/ugd/2fc31c_88705ccc71364e7f9d07408fc53401ec.pdf",
+    Typ.Kreis
   )
 );
 
 links.push(
   new ConstitutionLink(
     "AfD BV Arnsberg Satzung",
-    "https://afd-bezirk-arnsberg.de/wp-content/uploads/sites/14/2019/11/Satzung-BV-Arnsberg-Stand-2019.pdf"
+    "https://afd-bezirk-arnsberg.de/wp-content/uploads/sites/14/2019/11/Satzung-BV-Arnsberg-Stand-2019.pdf",
+    Typ.Bezirk
   )
 );
 
 links.push(
   new ConstitutionLink(
     "AfD LV NRW Landessatzung",
-    "https://nrw.afd-hosting.de/wp-content/uploads/sites/2/2020/01/AfD-NRW-Landessatzung-Stand-2020-01-18.pdf"
+    "https://nrw.afd-hosting.de/wp-content/uploads/sites/2/2020/01/AfD-NRW-Landessatzung-Stand-2020-01-18.pdf",
+    Typ.Land
   )
 );
 
 links.push(
   new ConstitutionLink(
     "AfD LV NRW Wahlordnung",
-    "https://nrw.afd-hosting.de/wp-content/uploads/sites/2/2020/01/AfD-NRW-Wahlordnung-Stand-2020-01-18.pdf"
+    "https://nrw.afd-hosting.de/wp-content/uploads/sites/2/2020/01/AfD-NRW-Wahlordnung-Stand-2020-01-18.pdf",
+    Typ.Land
   )
 );
 
 links.push(
   new ConstitutionLink(
     "AfD LV NRW Geschäftsordnung",
-    "https://nrw.afd-hosting.de/wp-content/uploads/sites/2/2015/11/AfD-NRW-Geschaeftsordnung-Stand-15-11-15.pdf"
+    "https://nrw.afd-hosting.de/wp-content/uploads/sites/2/2015/11/AfD-NRW-Geschaeftsordnung-Stand-15-11-15.pdf",
+    Typ.Land
   )
 );
 
 links.push(
   new ConstitutionLink(
     "AfD LV NRW Finanzordnung",
-    "https://nrw.afd-hosting.de/wp-content/uploads/sites/2/2018/07/AfD-NRW-Finanzordnung-Stand-10.6.2018.pdf"
+    "https://nrw.afd-hosting.de/wp-content/uploads/sites/2/2018/07/AfD-NRW-Finanzordnung-Stand-10.6.2018.pdf",
+    Typ.Land
   )
 );
 
 links.push(
   new ConstitutionLink(
     "AfD Bundessatzung",
-    "https://www.afd.de/wp-content/uploads/2022/08/20220816_Bundessatzung_Stand-13.-BPT-Riesa.pdf"
+    "https://www.afd.de/wp-content/uploads/2022/08/20220816_Bundessatzung_Stand-13.-BPT-Riesa.pdf",
+    Typ.Bund
   )
 );
 
 links.push(
   new ConstitutionLink(
     "AfD Finanz-Beitragsordnung",
-    "https://www.afd.de/wp-content/uploads/2022/08/20220819_Finanz-_und_Beitragsordnung_Stand-13.-BPT-Riesa.pdf"
+    "https://www.afd.de/wp-content/uploads/2022/08/20220819_Finanz-_und_Beitragsordnung_Stand-13.-BPT-Riesa.pdf",
+    Typ.Bund
   )
 );
 
 links.push(
   new ConstitutionLink(
     "AfD Schiedsgerichtsordnung",
-    "https://www.afd.de/wp-content/uploads/2022/08/20220819_AfD_Schiedsgerichtsordnung_Stand-13.-BPT-Riesa.pdf"
+    "https://www.afd.de/wp-content/uploads/2022/08/20220819_AfD_Schiedsgerichtsordnung_Stand-13.-BPT-Riesa.pdf",
+    Typ.Bund
   )
 );
 
 links.push(
   new ConstitutionLink(
     "AfD Wahlordnung",
-    "https://www.afd.de/wp-content/uploads/2022/08/20220817_AfD_Wahlordnung_Stand-1.7.2021.pdf"
+    "https://www.afd.de/wp-content/uploads/2022/08/20220817_AfD_Wahlordnung_Stand-1.7.2021.pdf",
+    Typ.Bund
   )
 );
 
 links.push(
   new ConstitutionLink(
     "AfD Geschäftsordnung Parteitag",
-    "https://www.afd.de/wp-content/uploads/2019/10/20191009_Gesch%C3%A4ftsordnung_f%C3%BCr_Parteitage.pdf"
+    "https://www.afd.de/wp-content/uploads/2019/10/20191009_Gesch%C3%A4ftsordnung_f%C3%BCr_Parteitage.pdf",
+    Typ.Bund
   )
 );
